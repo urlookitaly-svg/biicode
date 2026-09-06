ae4106e8064217e17e7ee58088dd7f20f146ef78
urlookitaly-svg <urlook.italy@gmail.com>
2026-09-06 10:24:29 +0200

    Fix PWA cache version so updated index.html is loaded

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='biicode-v1';
+const CACHE='biicode-v2';
 const APP=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
